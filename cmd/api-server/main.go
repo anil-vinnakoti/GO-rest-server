6a744ebc0df35ef3ae084def7d46e3228650eefd
Anil Kumar Vinnakoti <anil.vinnakoti@gmail.com>
2026-01-24 23:36:03 +0530

    add structed logs and tests for the logger

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/anil-vinnakoti/newsapi/internal/router"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+
+	logger.Info("server starting on port 8080")
+
 	routesHandler := router.New()
 	if err := http.ListenAndServe(":8080", routesHandler); err != nil {
-		log.Fatal("failed to start the server:", err)
+		logger.Error("failed to start the server:", "error", err)
 	}
 }
---
+++ internal/logger/log_test.go
@@ -0,0 +1,72 @@
+package logger_test
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/anil-vinnakoti/newsapi/internal/logger"
+)
+
+func Test_CtxWithLogger(t *testing.T) {
+	testCases := []struct {
+		name           string
+		ctx            context.Context
+		logger         *slog.Logger
+		expectedLogger bool
+	}{
+		{
+			name: "return context without logger",
+			ctx:  context.Background(),
+		}, {
+			name:           "return ctx as it is",
+			ctx:            context.WithValue(context.Background(), logger.CtxKey{}, slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))),
+			expectedLogger: true,
+		}, {
+			name:           "inject logger",
+			ctx:            context.Background(),
+			logger:         slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true})),
+			expectedLogger: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			ctx := logger.CtxWithLogger(testCase.ctx, testCase.logger)
+
+			_, ok := ctx.Value(logger.CtxKey{}).(*slog.Logger)
+			if testCase.expectedLogger != ok {
+				t.Errorf("expected: %v, recieved: %v", testCase.expectedLogger, ok)
+			}
+		})
+	}
+}
+
+func Test_FromContext(t *testing.T) {
+	testCases := []struct {
+		name           string
+		ctx            context.Context
+		expectedLogger bool
+	}{
+		{
+			name:           "logger exists",
+			ctx:            context.WithValue(context.Background(), logger.CtxKey{}, slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))),
+			expectedLogger: true,
+		}, {
+			name:           "new logger returned",
+			ctx:            context.Background(),
+			expectedLogger: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			logger := logger.FromContext(testCase.ctx)
+
+			if testCase.expectedLogger && logger == nil {
+				t.Errorf("expected: %v, recieved: %v", testCase.expectedLogger, logger)
+			}
+		})
+	}
+}
